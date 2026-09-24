e3b585d8a95bfc0ac599cd2dc49ab8e22bfe92dc
SANWAR LAL BALAI <sanwarlalsalvi97@gmail.com>
2026-09-24 08:55:27 +0530

    Register GoogleAuth plugin in MainActivity
    Removed commented lines and added GoogleAuth plugin registration.

--- android/app/src/main/java/com/ashapurasamrat/app/MainActivity.java
+++ android/app/src/main/java/com/ashapurasamrat/app/MainActivity.java
@@ -2,16 +2,13 @@ package com.ashapurasamrat.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth; // 1. GoogleAuth Import जोड़ा गया
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // 2. सभी Plugins रजिस्टर किए गए
+
         registerPlugin(AdPrivacyPlugin.class);
         registerPlugin(NativePermissionsPlugin.class);
-        registerPlugin(GoogleAuth.class); // GoogleAuth Plugin यहाँ जोड़ा गया
     }
 }
