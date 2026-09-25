3dfbe603bee45b58bf1fca82e18d75dcff6c985b
SANWAR LAL BALAI <sanwarlalsalvi97@gmail.com>
2026-09-26 01:00:50 +0530

    Update Firebase configuration details
    Updated Firebase configuration with new API key and measurement ID.

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,14 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA1-XCSoOJtWjHa8lqIGcG1GiSZdPErc4",
-    authDomain: "ashapura-app-f8e6c.firebaseapp.com",
-      projectId: "ashapura-app-f8e6c",
-        storageBucket: "ashapura-app-f8e6c.appspot.com",
-          messagingSenderId: "1006500502499",
-            appId: "1:1006500502499:web:43f93bcd4520aec77d9edc"
-            };
+  apiKey: "AIzaSyAEl-XCSoOJtWjHa8lqLiGcLGiSZdPEzc4",
+  authDomain: "ashapura-app-f8e6c.firebaseapp.com",
+  projectId: "ashapura-app-f8e6c",
+  storageBucket: "ashapura-app-f8e6c.firebasestorage.app",
+  messagingSenderId: "1006500502499",
+  appId: "1:1006500502499:web:43f93bcd4520aec77d9ed6",
+  measurementId: "G-S94J9GHRJZ"
+};
 
-            const app = initializeApp(firebaseConfig);
-            export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
             
